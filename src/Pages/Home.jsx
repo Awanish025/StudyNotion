4acb73807d4e28befdd2ca60c76123dfea1bef33
Awanish <awanishverma025@gmail.com>
2025-08-20 15:47:04 +0530

    reactupdate

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,10 +16,10 @@ const Home = () => {
       <div className=" relative  mx-auto flex flex-col w-11/12 max-w-maxContent  items-center justify-between text-white  ">
         <Link to={"/signup"}>
           <div className=" group mx-auto mt-16 w-fit rounded-full bg-richblack-800 p-1 font-bold text-richblack-200 drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 hover:scale-95 hover:drop-shadow-none ">
-            <diV className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
+            <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
               <p>Become an Instructor </p>
               <FaArrowRight />
-            </diV>
+            </div>
           </div>
         </Link>
         <div>
