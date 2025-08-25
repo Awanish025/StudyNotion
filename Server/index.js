c8c9c7c1744144619eb72a88bb01a493878b0842
Awanish <awanishverma025@gmail.com>
2025-08-25 18:27:50 +0530

    origin

--- Server/index.js
+++ Server/index.js
@@ -29,11 +29,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000", 
-      "https://study-notion-yoj3-gilt.vercel.app"  // ✅ add deployed frontend URL
-    ],
+    // origin: [
+    //   "http://localhost:3000",
+      
+      
+    // ],
    // origin:"http://localhost:3000",
+     origin:"*",
    
     credentials: true,
   })
